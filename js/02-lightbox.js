bf3d338eb1b3ab9e545a5dbf425f200b2b8ea8f3
OleksandrKutsevych <alexander18091993@gmail.com>
2021-12-12 16:56:40 +0200

    add js

---
+++ js/02-lightbox.js
@@ -0,0 +1,25 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createCardMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createCardMarkup(cards) {
+  return cards
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+    })
+    .join("");
+}
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  /* options */
+  captionsData: "alt",
+  captionDelay: 250,
+  captionType: "attr",
+});
